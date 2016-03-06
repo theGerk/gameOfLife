4578f123a1f4b402218c3e70611114b05bcd4a91
the_gerk1 <benji@altmansoftwaredesign.com>
2016-03-06 01:29:26 -0600

    theorectically, this should be complete, but debugging....

--- GameOfLife.js
+++ GameOfLife.js
@@ -5,7 +5,20 @@ var standard = standard || {};
 
 var canvas = document.getElementById('interact');
 var render = canvas.getContext('2d');
-var colors = {foreground: 'black', background: 'white'};
+gameOfLife.colors = {
+	foreground: 'black',
+	background: 'white'
+};
+
+Math.getPeriod = function(input, func) {
+	var copy = standard.deepCopy(input);
+	var itr = 0;
+	do {
+		input = func(input);
+		itr++;
+	} while(!(standard.deepEqual(input, copy)));
+	return itr;
+};
 
 standard.deepCopy = function(input) {
 	//dealing with primitives
@@ -20,11 +33,40 @@ standard.deepCopy = function(input) {
 	return output;
 };
 
+standard.deepEqual = function(a, b) {
+	//dealling with primitives
+	if(!((a instanceof Object) && (b instanceof Object)))
+		return a === b;
+
+	//all other types
+	for(var i in a)
+		standard.deepEqual(a[i], b[i]);
+};
+
+standard.initMatrix = function(xSize, ySize, init) {
+	var tmp = new Array(ySize).fill(init);	//create one row of inner array
+	var output = new Array(xSize);			//create array for output of the correct length
+	
+	//fill output array with copies of inner array
+	for(var i = 0; i < xSize; i++) {
+		output[i] = tmp.slice();
+	}
+	
+	//return
+	return output;
+};
+
 specificPrompt.gen = function(initStr, correctionStr, paramater, initExample, correctionExample) {
 	var output = prompt(initStr, initExample);
 	while(!paramater(output))
 		output = prompt(correctionStr, correctionExample);
 	return output;
+};
+
+specificPrompt.naturalNumber = function(initStr, correctionStr, initExample, correctionExample) {
+	return +specificPrompt.gen(initStr, correctionStr, function(input) {
+		return Number.isInteger(+input) && input >= 0;
+	}, initExample, correctionExample);
 };
 
 specificPrompt.positiveInteger = function(initStr, correctionStr, initExample, correctionExample) {
@@ -33,9 +75,10 @@ specificPrompt.positiveInteger = function(initStr, correctionStr, initExample, c
 	}, initExample, correctionExample);
 };
 
-gameOfLife.runGrid = function(funct) {
+gameOfLife.start = function(funct) {
 	//set default funct to random
 	if(!(funct instanceof Function))
+		funct = gameOfLife.initGrid.rand;
 
 	//get size of cell
 	var size = specificPrompt.positiveInteger('Please enter size of cell in pixels.', 'Please enter a positive integer.');
@@ -47,45 +90,85 @@ gameOfLife.runGrid = function(funct) {
 	canvas.width = size * width;
 
 	//set up arrays
-	var grid = funct(height, width);
+	var grid = funct(width, height);
 	var grid2 = standard.deepCopy(grid);
 
 	//draw main grid to screen
-	drawBitMaxtrix(grid, render, colors, size);
+	drawBitMatrix(grid, render, colors, size);
 
 	//ask for speed
-	var speed = specificPrompt.positiveInteger('What speed would you like to run this at (in milliseconds)?', 'Please enter a number greater then or equal to 1.');
+	var speed = specificPrompt.naturalNumber('What speed would you like to run this at (in milliseconds per frame, 0 will go as fast as possible)?', 'Please enter a number greater then or equal to 0.');
 
 	//run simulation
-	runThrough(grid, grid2, render, colors, size, speed, 0, lifeOf(grid));
+	if(speed === 0)	//if speed is 0 run quick version
+		gameOfLife.runThroughFast(grid, grid2, render, colors, size, lifeOf(grid));
+	else
+		gameOfLife.runThrough(grid, grid2, render, colors, size, speed, 0, lifeOf(grid));
 };
 
-var drawBitMaxtrix = function(grid, element, color, size) {
+var drawBitMatrix = function(grid, element, color, pixelSize) {
+	//set up constants for function
+	var x_max = grid.length;
+	var y_max = grid[0].length;
+	
+	//resent background
 	element.fillStyle = color.background;
-	element.fillRect(0, 0, grid[0].length * size, grid.length * size);
+	element.fillRect(0, 0, x_max * pixelSize, y_max * pixelSize);
+	
+	//change to foreground color
 	element.fillStyle = color.foreground;
-	for(var i = 0; i < grid.length; i++) {
-		for(var j = 0; j < grid[i].length; j++) {
-			if(grid[i][j]) {
-				element.fillRect(j * size, i * size, size, size);
+	for(var x = 0; x < x_max; x++) {
+		var tmp = grid[x];
+		for(var y = 0; y < y_max; y++) {
+			if(tmp[y]) {
+				element.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
 			}
 		}
 	}
 };
 
-var runThrough = function(grid, backgroundGrid, element, color, size, speed, iterations, startNumb) {
+gameOfLife.runThroughFast = function(grid, backgroundGrid, element, color, size, startNumb) {
+	//get iterations between renders
+	var iterationsBetweenRender = specificPrompt.naturalNumber('Please enter number of iterations between rendering.\n(0 is reserved for no rendering.)', 'Number must be greater then or equal to 0');
+	
+	//run loop
+	var iterations = 0;
+	do {
+		//apply iteration to main grid, and two to background grid
+		grid = applyItteration(grid);
+		backgroundGrid = applyIteration(applyIteration(backgroundGrid));
+		
+		//draw to canvas
+		if(iterations % iterationsBetweenRender === 0)
+			drawBitMatrix(grid, element, color, size);
+	} while(!standard.deepEqual(grid, backgroundGrid));
+	
+	//output end information
+	gameOfLife.outputInfo(iterations, grid, startNumb);
+};
+
+gameOfLife.runThroughSlow = function(grid, backgroundGrid, element, color, size, speed, iterations, startNumb) {
+	//apply one iteration to main grid, and two to the background grid
 	grid = applyIteration(grid);
 	backgroundGrid = applyIteration(applyIteration(backgroundGrid));
-	drawBitMaxtrix(grid, element, color, size);
-	if(equal(grid, backgroundGrid))
-		alert("Hit consistancy at around " + iterations + " iterations.\nYou are on a period of: " + getPeriod(grid) + "\nYou started with " + startNumb + " and...\nthere are currently " + lifeOf(grid) + " cells out of " + grid.length * grid[0].length);
+	
+	//draw to canvas
+	drawBitMatrix(grid, element, color, size);
+	
+	//check if we've hit the end
+	if(standard.deepEqual(grid, backgroundGrid))
+		gameOfLife.outputInfo(iterations, grid, startNumb);
 	else
 		setTimeout(function(){
 			runThrough(grid, element, color, size, speed, backgroundGrid, iterations + 1, startNumb);
 		}, speed);
 };
 
-var lifeOf = function(input) {
+gameOfLife.outputInfo = function(iterations, grid, startNumb) {
+	alert('Hit stability at around ' + iterations + ' iterations.\nYou are on a period of: ' + gameOfLife.getPeriod(grid) + '\nYou started with ' + startNumb + 'and...\n there are currently ' + gameOfLife.lifeOf(grid) + ' cells out of ' + grid.length * grid[0].length);
+};
+
+gameOfLife.lifeOf = function(input) {
 	var output = 0;
 	for(var i = 0; i < input.length; i++)
 		for(var j = 0; j < input[i].length; j++)
@@ -94,25 +177,12 @@ var lifeOf = function(input) {
 	return output;
 };
 
-var getPeriod = function(start) {
-	var iter = applyIteration(start);
-	for(var output = 1; !equal(start, iter); output++)
-		iter = applyIteration(start);
-	return output;
+gameOfLife.getPeriod = function(start) {
+	return Math.getPeriod(start, gameOfLife.applyIteration);
 };
 
-
-var equal = function(a, b) {
-	for(var i = 0; i < a.length; i++)
-		for(var j = 0; j < a[i].length; j++)
-			if(a[i][j] !== b[i][j])
-				return false;
-	return true;
-};
-
-
-var initGrid = {
-	solidSquareInCenter: function(height, width) {
+gameOfLife.initGrid = {
+	solidSquareInCenter: function(width, height) {
 		var output = [];
 
 		var cords = {h: {start: Math.floor(height / 4), end: Math.ceil(3 * height / 4)},
@@ -120,9 +190,9 @@ var initGrid = {
 
 
 		//iterate for each bit
-		for(var h = 0; h < height; h++) {
+		for(var w = 0; w < width; w++) {
 			var addin = [];
-			for(var w = 0; w < width; w++) {
+			for(var h = 0; h < height; h++) {
 				addin.push(h > cords.h.start && h < cords.h.end && w > cords.w.start && w < cords.w.end);
 			}
 			output.push(addin);
@@ -131,16 +201,16 @@ var initGrid = {
 		return output;
 	},
 
-	solidCircle: function(height, width) {
+	solidCircle: function(width, height) {
 		var output = [];
 
 		var radius = (height < width) ? height / 3 : width / 3;
 		var center = {h: height / 2, w: width / 2};
 
 		//iterate for each bit
-		for(var h = 0; h < height; h++) {
+		for(var w = 0; w < width; w++) {
 			var addin = [];
-			for(var w = 0; w < width; w++) {
+			for(var h = 0; h < height; h++) {
 				addin.push((h - center.h) * (h - center.h) + (w - center.w) * (w - center.h) < radius * radius);
 			}
 			output.push(addin);
@@ -149,28 +219,28 @@ var initGrid = {
 		return output;
 	},
 
-	dead: function(height, width) {
+	dead: function(width, height) {
 		var output = [];
 
-		for(var h = 0; h < height; h++) {
+		for(var w = 0; w < width; w++) {
 			var addin = [];
-			addin.length = width;
+			addin.length = height;
 			output.push(addin);
 		}
 
 		return output;
 	},
 
-	randCircle: function(height, width) {
+	randCircle: function(width, height) {
 		var output = [];
 
 		var radius = (height < width) ? height / 3 : width / 3;
 		var center = {h: height / 2, w: width / 2};
 
 		//iterate for each bit
-		for(var h = 0; h < height; h++) {
+		for(var w = 0; w < width; w++) {
 			var addin = [];
-			for(var w = 0; w < width; w++) {
+			for(var h = 0; h < height; h++) {
 				addin.push((Math.random() < .5) && ((h - center.h) * (h - center.h) + (w - center.w) * (w - center.h) < radius * radius));
 			}
 			output.push(addin);
@@ -179,13 +249,13 @@ var initGrid = {
 		return output;
 	},
 
-	rand: function(height, width) {
+	rand: function(width, height) {
 		var output = [];
 
 		//iterate for each bit
-		for(var h = 0; h < height; h++) {
+		for(var w = 0; w < width; w++) {
 			var addin = [];
-			for(var w = 0; w < width; w++) {
+			for(var h = 0; h < height; h++) {
 				addin.push(Math.random() >= .5);
 			}
 			output.push(addin);
@@ -194,15 +264,15 @@ var initGrid = {
 		return output;
 	},
 
-	randSquareInCenter: function(height, width) {
+	randSquareInCenter: function(width, height) {
 		var output = [];
 
 		var cords = {h: {start: Math.floor(height / 4), end: Math.ceil(3 * height / 4)},
 					 w: {start: Math.floor(width / 4) , end: Math.ceil(3 * width / 4)}};
 
-		for(var h = 0; h < height; h++) {
+		for(var w = 0; w < width; w++) {
 			var addin = [];
-			for(var w = 0; w < width; w++) {
+			for(var h = 0; h < height; h++) {
 				addin.push(Math.random() < .5 && h > cords.h.start && h < cords.h.end && w > cords.w.start && w < cords.w.end);
 			}
 			output.push(addin);
@@ -211,59 +281,85 @@ var initGrid = {
 	}
 };
 
-
-/*
- * @param grid: array of arrays of booleans
- */
-var applyIteration = function(grid) {
-	var output = [];
-
-	//iterate through grid
-	for(var i = 0; i < grid.length; i++) {
-		var addin = [];
-		for(var j = 0; j < grid[i].length; j++) {
-			addin.push(compute(grid, i, j));
+gameOfLife.createNumberGrid = function(input) {
+	//create any constants needed
+	var I_MAX = input.length;
+	var J_MAX = input[0].length;
+	
+	//create output array with 0s everywhere
+	var output = standard.initMatrix(I_MAX, J_MAX, 0);
+	
+	//iterate through all integer vectors in the range [<-1, -1>, <1, 1>], not including <0, 0>
+	for(var x = -1; x <= 1; x++) {
+		for(var y = -1; y <= 1; y++) {
+			//skip <0, 0>
+			if(x === 0 && y === 0)
+				continue;
+			
+			//setup x and i initilizations
+			var iStart = 0, iMax = I_MAX;
+			if(x === -1)
+				iStart++;
+			else if(x === 1)
+				iMax--;
+			
+			//setup y and j inilitizations
+			var jStart = 0, jMax = J_MAX;
+			if( y === -1)
+				jStart++;
+			else if(x === 1)
+				jMax--;
+			
+			//for loops
+			for(i = iStart; i < iMax; i++) {
+				
+				//make temp grid (less derefrencing)
+				var tmpInput = input[i];
+				var tmpOut = output[i + x];
+				for(j = jStart; j < jMax; j++) {
+					tmpOut[j] += tmpInput[j + y];
+				}
+			}
 		}
-		output.push(addin);
 	}
+	
+	//give otuput
+	return output;
+};
+
+gameOfLife.applyIteration = function(grid) {
+	//create number grid
+	var numbers = gameOfLife.createNumberGrid(grid);
+	
+	//create max values for loop
+	var IMAX = grid.length, JMAX = grid[0].length;
+	
+	//create array to use for output
+	var output = new Array(IMAX);
+
+	for(var i = 0; i < IMAX; i++) {
+		var tmpNumb = numbers[i];
+		var tmpGrid = grid[i];
+		var tmpOutput = new Array(JMAX);
+		for(var j = 0; j < JMAX; j++) {
+			tmpOutput[j] = gameOfLife.compute(tmpGrid[j], tmpNumb[j]);
+		}
+		output[i] = tmpOutput;
+	}
+	
 	return output;
 };
 
 
-var compute = function(grid, x, y) {
-	var sum = summiate(grid, x, y);
-	if(!grid[x][y] && sum === 3)
-		return true;
-	else if(grid[x][y] && (sum === 3 || sum === 2))
-		return true;
+gameOfLife.compute = function(prevVal, numbAdjacent) {
+	if(prevVal)
+		if(numbAdjacent === 2 || numbAdjacent === 3)
+			return true;
+		else
+			return false;
 	else
-		return false;
-};
-
-var summiate = function(grid, x, y) {
-	var out = 0;
-	var xm = x - 1 >= 0;
-	var xp = x + 1 < grid.length;
-	var ym = y - 1 >= 0;
-	var yp = y + 1 < grid[0].length;
-	if(xm) {
-		out += grid[x - 1][y];
-		if(ym)
-			out += grid[x - 1][y - 1];
-		if(yp)
-			out += grid[x - 1][y + 1];
-	}
-	if(xp) {
-		out += grid[x + 1][y];
-		if(ym)
-			out += grid[x + 1][y - 1];
-		if(yp)
-			out += grid[x + 1][y + 1];
-	}
-	if(ym)
-		out += grid[x][y - 1];
-	if(yp)
-		out += grid[x][y + 1];
-
-	return out;
+		if(numbAdjacent === 3)
+			return true;
+		else
+			return false;
 };
